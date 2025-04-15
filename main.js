609564a164d0ddbccea81d76602b5ed986a4e805
Kauane Melis Schuelter <147657430+kauschuelter12@users.noreply.github.com>
2025-04-15 13:32:47 -0300

    ,

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-16T00:00:00");
-const tempoObjetivo2 = new Date("2025-02-10T00:00:00");
+const tempoObjetivo2 = new Date("2026-02-10T00:00:00");
 const tempoObjetivo3 = new Date("2025-11-09T00:00:00");
 const tempoObjetivo4 = new Date("2025-06-01T00:00:00");
 
